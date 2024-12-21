61c4f41979e8669584883cbe4f95f4ddce5d7523
haakel <haakel213@gmail.com>
2024-12-21 11:22:56 +0330

    s16 - e01

--- test1/functions.php
+++ test1/functions.php
@@ -1,8 +1,23 @@
 <?php
 // echo "hi";
 
+// global $wpdb;
+// echo '<pre>';
+// var_dump($wpdb);
+// echo '<pre>';
+// exit;
+
+//CRUD
+
+
 global $wpdb;
+$R = $wpdb->get_results("SELECT * FROM $wpdb->users");
+// $R = $wpdb->get_results("SELECT * FROM $wpdb->users",ARRAY_A);
+ echo '<pre>';
+ var_dump($R);
 echo '<pre>';
-var_dump($wpdb);
-echo '<pre>';
-exit;
+// echo $R[0]->user_pass; 
+//  echo $R[0]["user_pass"];
+foreach($R as $item){
+    echo $item->user_login;
+}
