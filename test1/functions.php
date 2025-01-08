71b11bab568ec287ca895faf81f5d5ce6b1badc6
haakel <haakel213@gmail.com>
2025-01-08 10:34:00 +0330

    s16 - e02

--- test1/functions.php
+++ test1/functions.php
@@ -9,15 +9,26 @@
 
 //CRUD
 
+// s16 -e01
+// global $wpdb;
+// $R = $wpdb->get_results("SELECT * FROM $wpdb->users");
+// // $R = $wpdb->get_results("SELECT * FROM $wpdb->users",ARRAY_A);
+//  echo '<pre>';
+//  var_dump($R);
+// echo '<pre>';
+// // echo $R[0]->user_pass; 
+// //  echo $R[0]["user_pass"];
+// foreach($R as $item){
+//     echo $item->user_login;
+// }
 
+
+//s16- e02
 global $wpdb;
-$R = $wpdb->get_results("SELECT * FROM $wpdb->users");
-// $R = $wpdb->get_results("SELECT * FROM $wpdb->users",ARRAY_A);
+//  echo $wpdb->prefix."testamozesh";
+$table_user = $wpdb->prefix."testamozesh";
+//$R = $wpdb->get_results("SELECT * FROM {$table_user}");
+$R = $wpdb->get_row("SELECT * FROM {$table_user}");
+  echo '<pre>';
+  var_dump($R);
  echo '<pre>';
- var_dump($R);
-echo '<pre>';
-// echo $R[0]->user_pass; 
-//  echo $R[0]["user_pass"];
-foreach($R as $item){
-    echo $item->user_login;
-}
